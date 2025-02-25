a429b6ef3106e06b4452f944060320eccc1ef021
Bhoj Narayan Nayak <bhojnayak18@gmail.com>
2025-02-25 19:39:13 +0530

    Make the search bar interactive for home page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,29 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid(){
     let productsHTML = '';
 
-    products.forEach((product) => {
+    const url = new URL(window.location.href);
+    const search = url.searchParams.get('search');
+
+    let filteredProducts = products;
+
+    // If a search exists in the URL parameters,
+    // filter the products that match the search.
+    if (search) {
+        filteredProducts = products.filter((product) => {
+            let matchingKeyword = false;
+
+            product.keywords.forEach((keyword) => {
+              if (keyword.toLowerCase().includes(search.toLowerCase())) {
+                matchingKeyword = true;
+              }
+            });
+      
+            return matchingKeyword ||
+              product.name.toLowerCase().includes(search.toLowerCase());
+        });
+    }
+
+    filteredProducts.forEach((product) => {
         productsHTML += `
             <div class="product-container">
                 <div class="product-image-container">
@@ -105,5 +127,17 @@ function renderProductsGrid(){
             // Reset the quantity selector to 1
             quantitySelector.value = "1";
         });
+    });
+
+    document.querySelector('.js-search-button').addEventListener('click', () => {
+        const search = document.querySelector('.js-search-bar').value;
+        window.location.href = `amazon.html?search=${search}`;
+    });
+
+    document.querySelector('.js-search-bar').addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            const search = document.querySelector('.js-search-bar').value;
+            window.location.href = `amazon.html?search=${search}`;
+        }
     });
 }
