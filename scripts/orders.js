3819dba9633a308a77d98392102d202f09a7d9b1
Bhoj Narayan Nayak <bhojnayak18@gmail.com>
2025-02-25 16:46:55 +0530

    Make the orders page interactive

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,6 +2,7 @@ import { getProduct, loadProductsFetch } from "../data/products.js";
 import { orders } from "../data/orders.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import formatCurrency from './utils/money.js';
+import { addToCart } from '../data/cart.js'
 
 async function loadPage() {
     await loadProductsFetch();
@@ -59,7 +60,7 @@ async function loadPage() {
                       }
                     </div>
                     <div class="product-quantity">Quantity: ${productDetails.quantity}</div>
-                    <button class="buy-again-button button-primary">
+                    <button class="buy-again-button button-primary js-buy-again" data-product-id="${product.id}">
                         <img class="buy-again-icon" src="images/icons/buy-again.png" />
                         <span class="buy-again-message">Buy it again</span>
                     </button>
@@ -79,6 +80,21 @@ async function loadPage() {
     }
 
     document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+
+    document.querySelectorAll('.js-buy-again').forEach((buyAgainButton) => {
+        buyAgainButton.addEventListener('click', () => {
+            const {productId} = buyAgainButton.dataset;
+            addToCart(productId, 1);
+
+            buyAgainButton.innerHTML = 'Added';
+            setTimeout(() => {
+                buyAgainButton.innerHTML = `
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+                `;
+            }, 1000);
+        });
+    });
 }
 
 loadPage();
