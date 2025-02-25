6be8e1917b1b2772c9190d10020e90772d2e802e
Bhoj Narayan Nayak <bhojnayak18@gmail.com>
2025-02-25 20:21:44 +0530

    Fix cart quantity of tracking and orders page

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,7 +2,7 @@ import { getProduct, loadProductsFetch } from "../data/products.js";
 import { orders } from "../data/orders.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import formatCurrency from './utils/money.js';
-import { addToCart } from '../data/cart.js'
+import { addToCart, calculateCartQuantity } from '../data/cart.js'
 
 async function loadPage() {
     await loadProductsFetch();
@@ -95,6 +95,8 @@ async function loadPage() {
             }, 1000);
         });
     });
+
+    document.querySelector('.js-cart-quantity').textContent = calculateCartQuantity();
 }
 
 loadPage();
--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,6 +1,7 @@
 import {getOrder} from '../data/orders.js';
 import {getProduct, loadProductsFetch} from '../data/products.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {calculateCartQuantity} from '../data/cart.js';
 
 async function loadPage() {
   await loadProductsFetch();
@@ -67,6 +68,8 @@ async function loadPage() {
   `;
 
   document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
+
+  document.querySelector('.js-cart-quantity').textContent = calculateCartQuantity();
 }
 
 loadPage();
